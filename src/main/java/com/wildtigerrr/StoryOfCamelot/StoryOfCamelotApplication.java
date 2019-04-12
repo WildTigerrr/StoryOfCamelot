ce120876e1db05bc8466a558826e66742e7959e4
Litovchenko Andrey <wildtigerrr@gmail.com>
2019-04-12 12:07:17 +0300

    Initial commit

---
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/StoryOfCamelotApplication.java
@@ -0,0 +1,22 @@
+package com.wildtigerrr.StoryOfCamelot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
+public class StoryOfCamelotApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoryOfCamelotApplication.class, args);
+    }
+
+}
