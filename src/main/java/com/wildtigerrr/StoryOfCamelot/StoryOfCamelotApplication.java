9007865bcb01ae3eb118d8cf02f6fe2f4021ec51
WildTigerrr <wildtigerrr@gmail.com>
2019-04-14 23:46:11 +0300

    Removed Scan

--- src/main/java/com/wildtigerrr/StoryOfCamelot/StoryOfCamelotApplication.java
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/StoryOfCamelotApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
 
 @Configuration
-@ComponentScan
+//@ComponentScan
 @EnableAutoConfiguration
 @Controller
 @SpringBootApplication
