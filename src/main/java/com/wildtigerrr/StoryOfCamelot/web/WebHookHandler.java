ca2211627bc9ebbfcb460549d31d1cc6ab6f6b2b
WildTigerrr <wildtigerrr@gmail.com>
2019-04-15 12:35:55 +0300

    Commented webhook

---
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/web/WebHookHandler.java
@@ -0,0 +1,39 @@
+package com.wildtigerrr.StoryOfCamelot.web;
+
+
+
+public class WebHookHandler {/* extends TelegramWebhookBot {
+
+import com.wildtigerrr.StoryOfCamelot.SOCBotConfig;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+    @Override
+    public BotApiMethod onWebhookUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(update.getMessage().getChatId().toString());
+            sendMessage.setText("Well, all information looks like noise until you break the code.");
+            return sendMessage;
+        }
+        return null;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return SOCBotConfig.WEBHOOK_USER;
+    }
+
+    @Override
+    public String getBotToken() {
+        return SOCBotConfig.WEBHOOK_TOKEN;
+    }
+
+    @Override
+    public String getBotPath() {
+        return SOCBotConfig.WEBHOOK_USER; //arbitrary path to deliver updates on, username is an example.
+    }
+*/
+}
