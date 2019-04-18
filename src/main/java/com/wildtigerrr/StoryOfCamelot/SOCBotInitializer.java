44294b7a4c8f7ff8d2cea51475d930c0593b18ec
WildTigerrr <wildtigerrr@gmail.com>
2019-04-18 22:05:35 +0300

    Autowired DB Interaction class

--- src/main/java/com/wildtigerrr/StoryOfCamelot/SOCBotInitializer.java
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/SOCBotInitializer.java
@@ -1,6 +1,7 @@
 package com.wildtigerrr.StoryOfCamelot;
 
 import com.wildtigerrr.StoryOfCamelot.web.WebHookHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,12 +11,12 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @RestController
 public class SOCBotInitializer {
 
+    @Autowired
     private WebHookHandler handler;
 
     @RequestMapping(value = "/webhook", method = RequestMethod.POST)
     public void webhook(@RequestBody Update update) {
         System.out.println("----------------HANDLER-----------------");
-        if (handler == null) handler = new WebHookHandler();
         handler.onWebhookUpdateReceived(update);
     }
 
