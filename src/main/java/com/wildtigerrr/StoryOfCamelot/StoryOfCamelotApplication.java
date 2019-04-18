3d88e2074941132698bb36168a231e442eb7cd3c
WildTigerrr <wildtigerrr@gmail.com>
2019-04-18 21:11:45 +0300

    Spring example implementation

--- src/main/java/com/wildtigerrr/StoryOfCamelot/StoryOfCamelotApplication.java
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/StoryOfCamelotApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 
+@Controller
 @SpringBootApplication
 @ComponentScan("com.wildtigerrr.StoryOfCamelot.database.schema")
 public class StoryOfCamelotApplication {
