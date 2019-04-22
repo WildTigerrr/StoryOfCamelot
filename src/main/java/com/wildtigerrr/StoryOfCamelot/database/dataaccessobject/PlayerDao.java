f30691c6e3dad5bdbc7c149c3cd5024cbe1a4139
WildTigerrr <wildtigerrr@gmail.com>
2019-04-22 19:06:45 +0300

    Dao property naming fix

--- src/main/java/com/wildtigerrr/StoryOfCamelot/database/dataaccessobject/PlayerDao.java
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/database/dataaccessobject/PlayerDao.java
@@ -7,8 +7,8 @@ import org.springframework.data.repository.query.Param;
 
 public interface PlayerDao extends CrudRepository<Player, Integer> {
 
-    @Query("SELECT p FROM Player p WHERE p.external_id = :external_id")
-    Player findByExternalId(@Param("external_id") String externalId);
+    @Query("SELECT p FROM Player p WHERE p.externalId = :externalId")
+    Player findByExternalId(@Param("externalId") String externalId);
 
     Player findByNickname(String nickname);
 
