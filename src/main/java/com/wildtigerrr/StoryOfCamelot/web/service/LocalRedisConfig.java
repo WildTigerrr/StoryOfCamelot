77027149d7b97d288787776698be016d9bb58f75
WildTigerrr <wildtigerrr@gmail.com>
2019-05-05 23:43:03 +0300

    Jedis pool method

--- src/main/java/com/wildtigerrr/StoryOfCamelot/web/service/LocalRedisConfig.java
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/web/service/LocalRedisConfig.java
@@ -5,7 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class LocalRedisConfig {
@@ -20,6 +25,21 @@ public class LocalRedisConfig {
         poolConfig.setTestOnReturn(true);
         poolConfig.setTestWhileIdle(true);
         return new JedisConnectionFactory(poolConfig);
+    }
+
+    @Bean
+    public JedisPool getJedisPool() {
+        try {
+            URI redisURI = new URI(System.getenv("REDISTOGO_URL"));
+            return new JedisPool(new JedisPoolConfig(),
+                    redisURI.getHost(),
+                    redisURI.getPort(),
+                    Protocol.DEFAULT_TIMEOUT,
+                    redisURI.getUserInfo().split(":",2)[1]);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Redis couldn't be configured from URL in REDISTOGO_URL env var:"+
+                    System.getenv("REDISTOGO_URL"));
+        }
     }
 
     @Bean
