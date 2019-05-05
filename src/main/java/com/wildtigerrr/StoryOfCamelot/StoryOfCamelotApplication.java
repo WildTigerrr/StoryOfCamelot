690b431cdecab585cec64ccc6c80a276c2302b10
WildTigerrr <wildtigerrr@gmail.com>
2019-05-06 00:59:17 +0300

    Properties on startup

--- src/main/java/com/wildtigerrr/StoryOfCamelot/StoryOfCamelotApplication.java
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/StoryOfCamelotApplication.java
@@ -9,14 +9,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.DefaultPropertiesPersister;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.util.Properties;
 
 @Controller
 @SpringBootApplication
-@Import(LocalRedisConfig.class)
+//@Import(LocalRedisConfig.class)
 public class StoryOfCamelotApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(StoryOfCamelotApplication.class, args);
+        addRedisProperties();
         new ResponseHandler().sendMessageToAdmin("Bot Started");
         addShutdownHook();
     }
@@ -28,6 +36,28 @@ public class StoryOfCamelotApplication {
             new ResponseHandler().sendMessageToAdmin("Bot Shutting Down");
             System.out.println("Shutdown Hook Added");
         }));
+    }
+
+    private static void addRedisProperties() {
+        try {
+            URI redisURI = new URI(System.getenv("REDIS_URL"));
+            Properties props = new Properties();
+            props.setProperty("spring.data.redis.repositories.enabled", "true");
+            props.setProperty("spring.redis.host", redisURI.getHost());
+            props.setProperty("spring.redis.password", redisURI.getUserInfo().split(":",2)[1]);
+            props.setProperty("spring.redis.ssl", "true");
+//            props.setProperty("spring.redis.pool.max-active", "10");
+//            props.setProperty("spring.redis.pool.max-idle", "5");
+            props.setProperty("spring.redis.pool.max-wait", "30000");
+            props.setProperty("spring.redis.port", String.valueOf(redisURI.getPort()));
+            File f = new File("application.properties");
+            OutputStream out = new FileOutputStream( f );
+            // write into it
+            DefaultPropertiesPersister p = new DefaultPropertiesPersister();
+            p.store(props, out, "Redis Settings");
+        } catch (Exception e ) {
+            e.printStackTrace();
+        }
     }
 
 }
