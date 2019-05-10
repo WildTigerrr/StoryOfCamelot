78233f595f505c5be7d0c227105aee4ade77c633
WildTigerrr <wildtigerrr@gmail.com>
2019-05-10 03:36:04 +0300

    Movement as status

--- src/main/java/com/wildtigerrr/StoryOfCamelot/web/ResponseHandler.java
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/web/ResponseHandler.java
@@ -172,12 +172,12 @@ public class ResponseHandler {
                 }
                 break;
             case MOVE:
-                if ((message.getPlayer().getStatus() == PlayerStatus.MOVEMENT)) {
-                    messages.sendMessage(MainText.ALREADY_MOVING.text(), message.getUserId());
-                } else if (!message.isQuery() || commandParts.length < 2) {
-                    movementService.sendAvailableLocations(message.getPlayer());
-                } else {
+                if (message.isQuery()) {
                     movementService.moveToLocation(message, commandParts[1]);
+                } else if (message.getPlayer().getStatus() == PlayerStatus.MOVEMENT) {
+                    messages.sendMessage(MainText.ALREADY_MOVING.text(), message.getUserId());
+                } else if (commandParts.length < 2) {
+                    movementService.sendAvailableLocations(message.getPlayer());
                 }
                 break;
             case SEND:
