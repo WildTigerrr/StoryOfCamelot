404ee00348c9d3fe75da87ad1ff93d6c1d3c429e
WildTigerrr <wildtigerrr@gmail.com>
2019-08-23 00:19:10 +0300

    Properties Russian Locale to ru_RU

--- src/main/java/com/wildtigerrr/StoryOfCamelot/bin/enums/Language.java
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/bin/enums/Language.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 
 public enum Language {
     ENG ( Emoji.FLAG_ENGLAND.getCode() + " English", Locale.ENGLISH),
-    RUS ( Emoji.FLAG_RUSSIA.getCode() + " Русский", new Locale("RU")),
+    RUS ( Emoji.FLAG_RUSSIA.getCode() + " Русский", new Locale("ru", "RU")),
     UKR ( Emoji.FLAG_UKRAINE.getCode() + " Українська", new Locale("uk", "UA"));
 
     private final String name;
