b7eed36d522e0bd39109b0f0bc4d281ad210c3e3
WildTigerrr <wildtigerrr@gmail.com>
2019-08-22 23:58:35 +0300

    Locale resolver for default locale; UTF-8 encoding

--- src/main/java/com/wildtigerrr/StoryOfCamelot/bin/config/TranslationConfig.java
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/bin/config/TranslationConfig.java
@@ -1,15 +1,29 @@
 package com.wildtigerrr.StoryOfCamelot.bin.config;
 
+import com.wildtigerrr.StoryOfCamelot.bin.enums.Language;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class TranslationConfig {
+
     @Bean
     public ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
-        source.setBasenames("message");
-        return source;
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Language.getDefaultLocale());
+        return localeResolver;
     }
 }
