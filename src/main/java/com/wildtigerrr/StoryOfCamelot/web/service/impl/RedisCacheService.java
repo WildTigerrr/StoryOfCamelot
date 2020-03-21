23614b8e428f59c1d9ba68f7a95063581adcd386
WildTigerrr <wildtigerrr@gmail.com>
2020-03-21 17:33:26 +0200

    Move cache clear to proper method

--- src/main/java/com/wildtigerrr/StoryOfCamelot/web/service/impl/RedisCacheService.java
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/web/service/impl/RedisCacheService.java
@@ -2,6 +2,7 @@ package com.wildtigerrr.StoryOfCamelot.web.service.impl;
 
 import com.wildtigerrr.StoryOfCamelot.web.service.CacheProvider;
 import com.wildtigerrr.StoryOfCamelot.web.service.CacheType;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.core.HashOperations;
@@ -12,6 +13,7 @@ import javax.annotation.PostConstruct;
 import java.util.Map;
 import java.util.Set;
 
+@Log4j2
 @Repository
 @Profile("!test")
 public class RedisCacheService implements CacheProvider {
@@ -25,12 +27,12 @@ public class RedisCacheService implements CacheProvider {
     @PostConstruct
     private void init(){
         hashOperations = redisTemplate.opsForHash();
+        clearCache(); // TODO Remove
     }
 
     @Override
     public void add(CacheType channel, Object key, Object object) {
         hashOperations.put(channel.key(), String.valueOf(key), object);
-        clearCache(); // TODO Remove
     }
 
     @Override
@@ -58,7 +60,9 @@ public class RedisCacheService implements CacheProvider {
     @Override
     public void clearChannel(CacheType channel) {
         Set<String> keys = hashOperations.keys(channel.key());
+        log.warn(keys);
         for (String key : keys) {
+            log.warn(key);
             delete(channel, key);
         }
     }
