d38a2f41c3a7564de0342448fb34f45965247c66
WildTigerrr <wildtigerrr@gmail.com>
2020-03-25 21:03:39 +0200

    Update backpack message

--- src/main/java/com/wildtigerrr/StoryOfCamelot/bin/base/GameMain.java
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/bin/base/GameMain.java
@@ -178,6 +178,7 @@ public class GameMain {
     private void getBackpack(UpdateWrapper message) {
         Backpack backpack = backpackService.findByPlayerId(message.getPlayer().getId());
         StringBuilder builder = new StringBuilder();
+        builder.append(translation.getMessage("player.backpack.info", message));
         backpack.getItems().forEach(item -> builder.append(item.backpackInfo(translation)));
         messages.sendMessage(TextResponseMessage.builder()
                 .text(builder.toString())
