e16593573cc6e410f772a04a38b576f2b9cc2936
WildTigerrr <wildtigerrr@gmail.com>
2020-04-19 02:33:45 +0300

    Starting async services on after ApplicationContextProvider init

--- src/main/java/com/wildtigerrr/StoryOfCamelot/bin/service/AsynchronousService.java
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/bin/service/AsynchronousService.java
@@ -2,6 +2,7 @@ package com.wildtigerrr.StoryOfCamelot.bin.service;
 
 import com.wildtigerrr.StoryOfCamelot.web.UpdateReceiver;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,7 @@ import javax.annotation.PostConstruct;
 
 @Service
 @Log4j2
+@DependsOn("applicationContextProvider")
 public class AsynchronousService {
 
     private final TaskExecutor executor;
