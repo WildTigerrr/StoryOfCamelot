d6514030910b1dd0be384e15f95c8683086206da
WildTigerrr <wildtigerrr@gmail.com>
2020-04-20 02:35:16 +0300

    UpdateReceiver exception handling

--- src/main/java/com/wildtigerrr/StoryOfCamelot/web/UpdateReceiver.java
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/web/UpdateReceiver.java
@@ -1,5 +1,6 @@
 package com.wildtigerrr.StoryOfCamelot.web;
 
+import com.wildtigerrr.StoryOfCamelot.web.service.ResponseManager;
 import com.wildtigerrr.StoryOfCamelot.web.service.message.IncomingMessage;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -14,26 +15,43 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class UpdateReceiver implements Runnable {
 
     private final ResponseHandler responseHandler;
+    private final ResponseManager messages;
 
-    public final Queue<IncomingMessage> messages = new ConcurrentLinkedQueue<>();
+    public final Queue<IncomingMessage> messageQueue = new ConcurrentLinkedQueue<>();
     private boolean active;
 
-    public UpdateReceiver(ResponseHandler responseHandler) {
+    public UpdateReceiver(ResponseHandler responseHandler, ResponseManager messages) {
         this.responseHandler = responseHandler;
+        this.messages = messages;
     }
 
     @Override
     public void run() {
         active = true;
-        while(active) {
-            for (IncomingMessage message = messages.poll(); message != null; message = messages.poll()) {
-                responseHandler.proceed(message);
+        while (active) {
+            for (IncomingMessage message = messageQueue.poll(); message != null; message = messageQueue.poll()) {
+                try {
+                    responseHandler.proceed(message);
+                } catch (Exception e) {
+                    onError(message, e);
+                }
             }
         }
     }
 
     public void process(Update update) {
-        messages.add(IncomingMessage.from(update));
+        messageQueue.add(IncomingMessage.from(update));
+    }
+
+    private void onError(IncomingMessage message, Exception e) {
+        sendErrorReport(
+                "Exception during runtime: `" + e.getMessage() + "`; " +
+                        "\n\nDuring working on message: `" + message + "`", e);
+    }
+
+    private void sendErrorReport(String message, Exception e) {
+        log.error(message, e);
+        messages.postMessageToAdminChannel(message, true);
     }
 
     @PreDestroy
