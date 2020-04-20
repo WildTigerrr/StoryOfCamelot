359e24e35d2fcdd764461fd9d17885731eaf0728
WildTigerrr <wildtigerrr@gmail.com>
2020-04-20 15:38:48 +0300

    Handle images

---
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/bin/handler/ImageMessageHandler.java
@@ -0,0 +1,35 @@
+package com.wildtigerrr.StoryOfCamelot.bin.handler;
+
+import com.wildtigerrr.StoryOfCamelot.bin.translation.TranslationManager;
+import com.wildtigerrr.StoryOfCamelot.web.BotConfig;
+import com.wildtigerrr.StoryOfCamelot.web.service.ResponseManager;
+import com.wildtigerrr.StoryOfCamelot.web.service.message.IncomingMessage;
+import com.wildtigerrr.StoryOfCamelot.web.service.message.template.ImageIncomingMessage;
+import com.wildtigerrr.StoryOfCamelot.web.service.message.template.ImageResponseMessage;
+import com.wildtigerrr.StoryOfCamelot.web.service.message.template.TextResponseMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageMessageHandler extends CommandHandler {
+
+    protected ImageMessageHandler(ResponseManager messages, TranslationManager translation) {
+        super(messages, translation);
+    }
+
+    @Override
+    public void process(IncomingMessage message) {
+        ImageIncomingMessage imageMessage = (ImageIncomingMessage) message;
+        messages.sendMessage(ImageResponseMessage.builder()
+                .targetId(BotConfig.ADMIN_CHANNEL_ID)
+                .caption(imageMessage.getAuthorCaption())
+                .fileId(imageMessage.getBiggetImageId())
+                .build()
+        );
+        messages.sendMessage(TextResponseMessage.builder()
+                .text(translation.getMessage("commands.unknown", message))
+                .targetId(message)
+                .build()
+        );
+    }
+
+}
