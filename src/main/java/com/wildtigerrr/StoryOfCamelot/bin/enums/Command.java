1e00f3b91ab0045402f43b0376dc876751fbf8da
WildTigerrr <wildtigerrr@gmail.com>
2020-04-20 14:43:37 +0300

    Add default fallback command

--- src/main/java/com/wildtigerrr/StoryOfCamelot/bin/enums/Command.java
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/bin/enums/Command.java
@@ -84,7 +84,8 @@ public enum Command {
             return true;
         }
     },
-    NOTIFY("notifyCommandHandler");
+    NOTIFY("notifyCommandHandler"),
+    DEFAULT;
 
     private final String handlerName;
 
