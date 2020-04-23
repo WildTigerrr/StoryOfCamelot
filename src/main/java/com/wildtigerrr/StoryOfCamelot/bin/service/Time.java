85d32ce2b3d6e57327d388d25807c45aee28673c
WildTigerrr <wildtigerrr@gmail.com>
2020-04-23 23:32:24 +0300

    Add Time utility class for conversion from milliseconds

---
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/bin/service/Time.java
@@ -0,0 +1,29 @@
+package com.wildtigerrr.StoryOfCamelot.bin.service;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Time {
+    
+    private final long MILLISECONDS_IN_SECOND = 1000;
+    private final long SECONDS_IN_MINUTE = 60;
+    private final long MINUTES_IN_HOUR = 60;
+    private final long HOURS_IN_DAY = 24;
+
+    public long days(long number) {
+        return hours(number) * HOURS_IN_DAY;
+    }
+
+    public long hours(long number) {
+        return minutes(number) * MINUTES_IN_HOUR;
+    }
+
+    public long minutes(long number) {
+        return seconds(number) * SECONDS_IN_MINUTE;
+    }
+    
+    public long seconds(long number) {
+        return number * MILLISECONDS_IN_SECOND;
+    }
+    
+}
