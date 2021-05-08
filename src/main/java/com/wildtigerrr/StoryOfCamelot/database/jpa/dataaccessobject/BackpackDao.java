3e9dbbc799daf7aa211e43d2c4e926ff8b492bcb
WildTigerrr <wildtigerrr@gmail.com>
2021-05-09 00:09:14 +0300

    Always retrieve backpack items with backpack by type

--- src/main/java/com/wildtigerrr/StoryOfCamelot/database/jpa/dataaccessobject/BackpackDao.java
+++ src/main/java/com/wildtigerrr/StoryOfCamelot/database/jpa/dataaccessobject/BackpackDao.java
@@ -2,11 +2,13 @@ package com.wildtigerrr.StoryOfCamelot.database.jpa.dataaccessobject;
 
 import com.wildtigerrr.StoryOfCamelot.database.jpa.schema.Backpack;
 import com.wildtigerrr.StoryOfCamelot.database.jpa.schema.enums.BackpackType;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 public interface BackpackDao extends CrudRepository<Backpack, String> {
     Optional<Backpack> findByPlayerId(String playerId);
+    @Query("SELECT b FROM Backpack b JOIN FETCH b.items WHERE b.player.id = :playerId AND b.type = :type")
     Optional<Backpack> findByPlayerIdAndType(String playerId, BackpackType type);
 }
